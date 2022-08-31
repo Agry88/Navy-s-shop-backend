0a853fde9cf65f1a1de0b75054114d1355ca1250
Agry88 <rgok307085@gmail.com>
2022-08-31 16:36:31 +0800

    Fix:Link fix

--- routes/members.js
+++ routes/members.js
@@ -40,7 +40,7 @@ router.post("/send", async (req, res) => {
             //主旨
             subject: '請點選連結來認證您在海軍商城的會員註冊', // Subject line
             //嵌入 html 的內文
-            html: `<h2>親愛的會員您好：<br />感謝您註冊海軍商城會員<br/>請點擊下方連結完成電子信箱認證<br /><a href="https://navys-shop-backend.herokuapp.com/api/verify/${create_token}">請點這裡</a><br />不是您本人嗎?請直接忽略或刪除此信件，<br />如有問題請撥打客服專線0800-024-550，謝謝！</h2>`,
+            html: `<h2>親愛的會員您好：<br />感謝您註冊海軍商城會員<br/>請點擊下方連結完成電子信箱認證<br /><a href="https://navys-shop-backend.herokuapp.com/api/members/verify/${create_token}">請點這裡</a><br />不是您本人嗎?請直接忽略或刪除此信件，<br />如有問題請撥打客服專線0800-024-550，謝謝！</h2>`,
         };
 
         //發送郵件
